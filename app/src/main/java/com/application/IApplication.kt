dd6ad709a53e9b089f444b43527d7f5cd119ccc3
258474063@qq.com <Quiet800>
2018-12-10 19:10:33 +0800

    add jni

--- app/src/main/java/com/application/IApplication.kt
+++ app/src/main/java/com/application/IApplication.kt
@@ -2,12 +2,15 @@ package com.application
 
 import android.content.Context
 import android.support.multidex.MultiDexApplication
+import com.utils.lib.ss.common.StrictModeHelper
 
 class IApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
         context = this
+
+        StrictModeHelper.initStrictMode()
     }
 
     companion object {
